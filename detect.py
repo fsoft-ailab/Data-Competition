43a75bfe5b29a17af8efc9b0e0da3b71a53e9804
nguye <nguyenngoclong1282001@gmail.com>
2021-09-28 15:02:30 +0700

    push source code

---
+++ detect.py
@@ -0,0 +1,196 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+Run inference on images, videos, directories, streams, etc.
+
+Usage:
+    $ python path/to/detect.py --source path/to/img.jpg --weights yolov5s.pt --img 640
+"""
+
+import argparse
+import sys
+from pathlib import Path
+
+import cv2
+import torch
+
+FILE = Path(__file__).resolve()
+sys.path.append(FILE.parents[0].as_posix())  # add yolov5/ to path
+
+from models.experimental import attempt_load
+from utils.datasets import LoadImages
+from utils.general import check_img_size, check_requirements, colorstr, is_ascii, \
+    non_max_suppression, scale_coords, xyxy2xywh, set_logging, increment_path, \
+    save_one_box
+from utils.plots import Annotator, colors
+from utils.torch_utils import select_device, time_sync
+
+
+@torch.no_grad()
+def run(weights,  # model.pt path(s)
+        source,  # file/dir/URL/glob, 0 for webcam
+        imgsz,  # inference size (pixels)
+        conf_thres,  # confidence threshold
+        iou_thres,  # NMS IOU threshold
+        max_det,  # maximum detections per image
+        device, # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img,  # show results
+        save_txt,  # save results to *.txt
+        save_conf,  # save confidences in --save-txt labels
+        save_crop,  # save cropped prediction boxes
+        nosave,  # do not save images/videos
+        classes,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms,  # class-agnostic NMS
+        augment,  # augmented inference
+        visualize,  # visualize features
+        project,  # save results to project/name
+        name,  # save results to project/name
+        exist_ok, # existing project/name ok, do not increment
+        line_thickness,  # bounding box thickness (pixels)
+        hide_labels,  # hide labels
+        hide_conf,  # hide confidences
+        half,  # use FP16 half-precision inference
+        ):
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+
+    # Directories
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(device)
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    w = weights[0] if isinstance(weights, list) else weights
+    suffix = Path(w).suffix.lower()
+    assert suffix == ".pt"
+
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    if half:
+        model.half()  # to FP16
+
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    ascii = is_ascii(names)  # names are ascii (use PIL for UTF-8)
+
+    # Dataloader
+    dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=True)
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.parameters())))  # run once
+    dt, seen = [0.0, 0.0, 0.0], 0
+    for path, img, im0s, _ in dataset:
+        t1 = time_sync()
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img = img / 255.0  # 0 - 255 to 0.0 - 1.0
+        if len(img.shape) == 3:
+            img = img[None]  # expand for batch dim
+        t2 = time_sync()
+        dt[0] += t2 - t1
+
+        # Inference
+
+        visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+        pred = model(img, augment=augment, visualize=visualize)[0]
+        t3 = time_sync()
+        dt[1] += t3 - t2
+
+        # NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        dt[2] += time_sync() - t3
+
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            imc = im0.copy() if save_crop else im0  # for save_crop
+            annotator = Annotator(im0, line_width=line_thickness, pil=not ascii)
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or save_crop or view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                        if save_crop:
+                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Print time (inference-only)
+            print(f'{s}Done. ({t3 - t2:.3f}s)')
+
+            im0 = annotator.result()
+            # Save results (image with detections)
+            if save_img:
+                cv2.imwrite(save_path, im0)
+
+    # Print results
+    t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
+    print(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        print(f"Results saved to {save_dir}")
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='pretrains/pretrain_5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default='dataset/images/public_test', help='folder contain image')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    opt = parser.parse_args()
+
+    opt.agnostic_nms = False
+    opt.augment = False
+    opt.classes = None
+    opt.exist_ok = False
+    opt.imgsz = [640, 640]
+    opt.nosave = False
+    opt.project = 'results/detect'
+    opt.view_img = False
+    opt.visualize = False
+    opt.max_det = 500
+    opt.line_thickness = 2
+    return opt
+
+
+def main(opt):
+    print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
+    check_requirements(exclude=('tensorboard', 'thop'))
+    run(**vars(opt))
+
+
+if __name__ == "__main__":
+    main(parse_opt())
